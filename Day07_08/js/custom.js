7ccbe7df177e677b781bd1291b5387a14474f143
DESKTOP-8UAQVME\Wai Linn Naing <wailinnaing.mtmintern@gmail.com>
2026-05-21 08:32:26 +0630

    Update With Clickable Slider

--- Day07_08/js/custom.js
+++ Day07_08/js/custom.js
@@ -1,7 +1,31 @@
-$(document).ready(function() {
-  $('.menu-bar').click(function() { 
+$(document).ready(function () {
+  $('.menu-bar').click(function () {
     $(this).toggleClass('is-active');
-     
+
     $('.nav-items').toggleClass('is-active');
   });
+
+  const $slides = $('.sliders .slider');
+  let currentSlide = 0; 
+  function showSlide(index) {
+    $slides.eq(currentSlide).removeClass('active');
+    if (index >= $slides.length) {
+      currentSlide = 0;
+    } else if (index < 0) {
+      currentSlide = $slides.length - 1;
+    } else {
+      currentSlide = index;
+    }
+    $slides.eq(currentSlide).addClass('active');
+  }
+ 
+  
+  $('.banner-click-btn.prev').click(function () {
+    showSlide(currentSlide - 1);  
+  });
+
+  $('.banner-click-btn.next').click(function () {
+    showSlide(currentSlide + 1);  
+  });
+ startAutoLoop();
 });
