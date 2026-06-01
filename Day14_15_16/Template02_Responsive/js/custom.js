e318dc0a50af793a4d5af265cf48a55da3cb8ffd
mtmintern-wailinnaing <wailinnaing.mtmintern@gmail.com>
2026-06-02 00:58:14 +0630

    remove common classes

--- Day14_15_16/Template02_Responsive/js/custom.js
+++ Day14_15_16/Template02_Responsive/js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $('.menu-bar').click(function () {
         $(this).toggleClass('is-active');
+        $('.nav').toggleClass('is-active');
 
         $('.nav-items').toggleClass('is-active');
     });
