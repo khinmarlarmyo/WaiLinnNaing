794598436f602247c3a9a2e1663c514c88c859cf
DESKTOP-8UAQVME\Wai Linn Naing <wailinnaing.mtmintern@gmail.com>
2026-06-03 16:29:07 +0630

    complete slider and scroll to top

--- Day14_15_16/Template02_Responsive/js/custom.js
+++ Day14_15_16/Template02_Responsive/js/custom.js
@@ -9,6 +9,19 @@ $(document).ready(function() {
     if ($('.menu-bar').hasClass('is-active')) {
       $('.menu-bar, .nav').removeClass('is-active');
     }
+   if ($(this).scrollTop() > 100) {
+          $('.scroll-top-wrapper').css('display', 'flex').fadeIn();
+        }
+        else {
+          $('.scroll-top-wrapper').fadeOut();
+        }
+  });
+
+  $('#scrollToTopBtn').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 600);  
+    return false;
   });
 
 });
